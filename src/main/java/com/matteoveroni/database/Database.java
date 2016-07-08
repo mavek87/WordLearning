9cf849168cad46761afbb576e5ba35e14fdaa5e9
Matteo Veroni <matver87@gmail.com>
2016-07-08 10:25:06 +0200

    try-with-resources used to close db connections instead of relying on apache db-utils.

--- src/main/java/com/matteoveroni/database/Database.java
+++ src/main/java/com/matteoveroni/database/Database.java
@@ -5,10 +5,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import org.apache.commons.dbutils.DbUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,56 +15,53 @@ import org.slf4j.LoggerFactory;
  */
 public class Database {
 
-    private volatile static Database databaseUniqueInstance;
-    private static final String DATABASE_PATH = App.PATH + File.separator + "Database" + File.separator + "database.sqlite";
-    private static final Logger LOG = LoggerFactory.getLogger(Database.class);
+	private volatile static Database databaseUniqueInstance;
+	private static final String DATABASE_PATH = App.PATH + File.separator + "Database" + File.separator + "database.sqlite";
+	private static final Logger LOG = LoggerFactory.getLogger(Database.class);
 
-    private Database() {
-    }
+	private Database() {
+	}
 
-    public static final Database getInstance() {
-        Database database_instance = databaseUniqueInstance;
-        if (database_instance == null) {
-            synchronized (Database.class) {
-                database_instance = databaseUniqueInstance;
-                if (database_instance == null) {
-                    databaseUniqueInstance = new Database();
-                }
-            }
-        }
-        return databaseUniqueInstance;
-    }
+	public static final Database getInstance() {
+		Database database_instance = databaseUniqueInstance;
+		if (database_instance == null) {
+			synchronized (Database.class) {
+				database_instance = databaseUniqueInstance;
+				if (database_instance == null) {
+					databaseUniqueInstance = new Database();
+				}
+			}
+		}
+		return databaseUniqueInstance;
+	}
 
-    public void createDb() {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        String sqlQuery = "CREATE TABLE 'Names' ('Id' INTEGER PRIMARY KEY  NOT NULL , 'Name' TEXT)";
-        LOG.info("Creating db, query => " + sqlQuery);
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
-            statement = connection.prepareStatement(sqlQuery);
-            statement.execute();
-            LOG.info("Database created successfully");
-        } catch (ClassNotFoundException | SQLException ex) {
-            LOG.error(ex.getClass().getName() + ": " + ex.getMessage());
-            System.exit(0);
-        } finally {
-            DbUtils.closeQuietly(statement);
-            DbUtils.closeQuietly(connection);
-        }
-    }
+	public void createDb() {
+		String sqlQuery = "CREATE TABLE 'Names' ('Id' INTEGER PRIMARY KEY  NOT NULL , 'Name' TEXT)";
+		LOG.info("Creating db, query => " + sqlQuery);
+		
+		try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
+			PreparedStatement statement = connection.prepareStatement(sqlQuery);) {
 
-    public Connection getConnection() throws SQLException {
-        Connection connection = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
-            LOG.info("Opened database successfully");
-        } catch (Exception ex) {
-            LOG.error(ex.getClass().getName() + ": " + ex.getMessage());
-            System.exit(0);
-        } 
-        return connection;
-    }
+			statement.execute();
+			LOG.info("Database created successfully");
+
+		} catch (SQLException ex) {
+			LOG.error(ex.getMessage());
+		}
+	}
+
+	public Connection getConnection() throws SQLException {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
+			LOG.info("Opened database successfully");
+		} catch (SQLException ex) {
+			LOG.error(ex.getClass().getName() + ": " + ex.getMessage());
+			if (connection != null) {
+				connection.close();
+			}
+			System.exit(0);
+		}
+		return connection;
+	}
 }
--- src/main/java/com/matteoveroni/views/dictionary/model/DictionaryDAO.java
+++ src/main/java/com/matteoveroni/views/dictionary/model/DictionaryDAO.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import org.apache.commons.dbutils.DbUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,37 +13,28 @@ import org.slf4j.LoggerFactory;
  * @author Matteo Veroni
  */
 public class DictionaryDAO {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DictionaryDAO.class);
-
-    public String getWord() throws SQLException {
-        Connection connection = Database.getInstance().getConnection();
-        Statement statement = null;
-        ResultSet resultSet = null;
-        String sqlQuery = "SELECT * FROM Names";
-        String word = "";
-        LOG.info("Get Word query => " + sqlQuery);
-
-        if (connection != null) {
-            try {
-                statement = connection.createStatement();
-                resultSet = statement.executeQuery(sqlQuery);
-
-                while (resultSet.next()) {
-                    String sResult = resultSet.getString("Name");
-                    LOG.debug(sResult);
-                }
-
-//            List ar = Arrays.asList(resultSet.getArray("Name"));
-//            System.out.println(ar.get(0));
-            } catch (Exception ex) {
-                LOG.error(ex.getClass().getName() + ": " + ex.getMessage());
-                System.exit(0);
-            } finally {
-                DbUtils.closeQuietly(connection, statement, resultSet);
-            }
-        }
-        return word;
-    }
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(DictionaryDAO.class);
+	
+	public String getWord() throws SQLException {
+		String sqlQuery = "SELECT * FROM Names";
+		String word = "";
+		LOG.info("Get Word query => " + sqlQuery);
+		
+		try (Connection connection = Database.getInstance().getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlQuery);) {
+			
+			while (resultSet.next()) {
+				String sResult = resultSet.getString("Name");
+				LOG.debug(sResult);
+			}
+			
+		} catch (SQLException ex) {
+			LOG.error(ex.getMessage());
+		}
+		
+		return word;
+	}
+	
 }
